1792d6c51b3145ecbf79aa02d1c6c0bf33f2f09d
Emilia Connelly <ecgconnelly@gmail.com>
2022-01-30 16:27:03 -0700

    add register names to AK9753 object

--- ak9753_sparkfun.py
+++ ak9753_sparkfun.py
@@ -3,15 +3,44 @@
 from smbus import SMBus
 import RPi.GPIO as GPIO
 
-from enum import Enum
+
 import time
 import random
 
 
-HPS_ADDR = 0x64
-HPS_PWR_PIN = 11 #physical pin
-
 class AK9753():
+	
+	class Registers():
+		WIA1 = 0x0
+		WIA2 = 0x1
+		INFO1 = 0x2
+		INFO2 = 0x3
+		INTST = 0x4
+		ST1 = 0x5
+		IR1L = 0x6
+		IR1H = 0x7
+		IR2L = 0x8
+		IR2H = 0x9
+		IR3L = 0xA
+		IR3H = 0xB
+		IR4L = 0xC
+		IR4H = 0xD
+		TMPL = 0xE
+		TMPH = 0xF
+		ST2 = 0x10
+		ETH13H_L = 0x11
+		ETH13H_H = 0x12
+		ETH13L_L = 0x13
+		ETH13L_H = 0x14
+		ETH24H_L = 0x15
+		ETH24H_H = 0x16
+		ETH24L_L = 0x17
+		ETH24L_H = 0x18
+		EHYS13 = 0x19
+		EHYS24 = 0x1A
+		EINTEN = 0x1B
+		ECNTL1 = 0x1C
+		CNTL2 = 0x1D
 	
 	def __init__(self, i2c_bus, i2c_address, 
 				 powerPin = None, intPin = None):
@@ -19,6 +48,7 @@ class AK9753():
 		self.i2c_address = i2c_address
 		self.powerPin = powerPin
 		self.intPin = intPin
+		
 	
 	def setStandbyMode(self):
 		self.write_emode(0)
@@ -29,10 +59,10 @@ class AK9753():
 	
 	def write_emode(self, emode_val):
 		#todo: make this less stupid, maybe store the EEPMODE/EFC values
-		ECNTL1 = self.read_register(0x1c)
+		ECNTL1 = self.read_register(self.Registers.ECNTL1)
 		mask = 0b11111000 + emode_val
 		ECNTL1 = ECNTL1 & mask
-		readback = self.write_register(0x1c, ECNTL1)
+		readback = self.write_register(self.Registers.ECNTL1, ECNTL1)
 		
 		if readback == ECNTL1:
 			print(f'Mode switch OK, EMODE now {emode_val}\n')
@@ -40,9 +70,6 @@ class AK9753():
 			print('Mode switch failed\n')
 		
 		
-
-		
-
 
 	def write_register(self, register, value, verify = True):
 		#writes any write-enabled register on the AK9753
@@ -69,12 +96,12 @@ class AK9753():
 		
 
 
-	def HPS_set_power(self, powerState):
+	def setPower(self, powerState):
 		if self.powerPin is None:
 			return #TODO: this should throw an exception?
 			
 		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(powerPin, GPIO.OUT)
+		GPIO.setup(self.powerPin, GPIO.OUT)
 		
 		# if we are bringing power on (and was off previously)
 		# then we need to make sure to wait for the AK9753 to 
@@ -97,7 +124,8 @@ def main():
 	
 	
 	i2c_bus = SMBus(1) #object representing our I2C bus
-	hps = AK9753(i2c_bus, HPS_ADDR)
+	hps = AK9753(i2c_bus, i2c_address = 0x64, powerPin = 11)
+	hps.setPower(True)
 	
 	hps.setSingleShotMode()
 	
